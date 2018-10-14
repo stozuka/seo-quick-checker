13526c90974aa945ff3333d08746fb95dfde21b6
stozuka <stozuka@gmail.com>
2018-10-14 21:03:10 +0800

    refactor the interface of fromStream

--- test/unit/get-dom/index.js
+++ test/unit/get-dom/index.js
@@ -1,15 +1,12 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 const { fromFile, fromStream } = require('../../../lib/get-dom');
 
 const assert = chai.assert;
-const expect = chai.expect;
 const BASE_PATH = path.resolve(__dirname, '../../fixtures/get-dom');
-
-chai.use(chaiAsPromised);
 
 describe('get-dom', () => {
   describe('from-file', function() {
@@ -19,22 +16,15 @@ describe('get-dom', () => {
       const h1Text = dom('h1').text();
       assert.equal(h1Text, 'test');
     });
-
-    it('should throw an error when filePath is missing', () => {
-      expect(fromFile()).to.be.rejected;
-    });
   });
 
   describe('from-stream', () => {
     it('should return the dom from stream', async () => {
       const filePath = path.resolve(BASE_PATH, 'index.html');
-      const dom = await fromStream(filePath);
+      const rs = fs.createReadStream(filePath, 'utf8');
+      const dom = await fromStream(rs);
       const h1Text = dom('h1').text();
       assert.equal(h1Text, 'test');
-    });
-
-    it('should throw an error when filePath is missing', () => {
-      assert.throws(() => fromStream());
     });
   });
 });
